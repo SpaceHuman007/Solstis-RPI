bb1a1da52d8bf7a58047f00a7697d6af76ce3218
SpaceHuman007 <ethancole02@gmail.com>
2025-09-14 16:49:39 -0400

    Update LEDeez.py

--- LEDeez.py
+++ LEDeez.py
@@ -37,7 +37,7 @@ strip.show()
 #    strip.setPixelColor(i, Color(0, 240, 255))
 
 # Range 3
-for i in range(40,50):
+for i in range(45,55):
     strip.setPixelColor(i, Color(0, 240, 255))
 
 strip.show()
