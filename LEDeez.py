185726ea280e2a6f6205d1bfb812150b035bd0ea
SpaceHuman007 <ethancole02@gmail.com>
2025-09-14 16:44:12 -0400

    Update LEDeez.py

--- LEDeez.py
+++ LEDeez.py
@@ -29,9 +29,20 @@ for i in range(strip.numPixels()):
         strip.setPixelColor(i, 0)
 strip.show()
 
-for i in range (650,700):
+for i in range (675,725):
         strip.setPixelColor(i,Color(0,240,255))
+
+# Range 2
+for i in range(400, 450):
+    strip.setPixelColor(i, Color(0, 240, 255))
+
+# Range 3
+for i in range(200, 250):
+    strip.setPixelColor(i, Color(0, 240, 255))
+
 strip.show()
+
+
 
 # keep on for 5 seconds
 time.sleep(5)
